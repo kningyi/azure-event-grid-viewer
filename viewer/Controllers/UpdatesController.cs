3dc9b7a1bf51444407142b9761820da53a38d371
Ningyi K <ningyi.kuo@sg.ey.com>
2023-12-22 05:19:39 +0800

    use request header to determine if it's cloud event

--- viewer/Controllers/UpdatesController.cs
+++ viewer/Controllers/UpdatesController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json;
@@ -71,7 +72,7 @@ namespace viewer.Controllers
                 {
                     // Check to see if this is passed in using
                     // the CloudEvents schema
-                    if (IsCloudEvent(jsonContent))
+                    if (IsCloudEvent())
                     {
                         return await HandleCloudEvent(jsonContent);
                     }
@@ -187,26 +188,14 @@ namespace viewer.Controllers
             return Ok();
         }
 
-        private static bool IsCloudEvent(string jsonContent)
+        private bool IsCloudEvent()
         {
-            // Cloud events are sent one at a time, while Grid events
-            // are sent in an array. As a result, the JObject.Parse will 
-            // fail for Grid events. 
-            try
+            if (!Request.Headers.ContainsKey("Content-Type"))
             {
-                // Attempt to read one JSON object. 
-                var eventData = JObject.Parse(jsonContent);
-
-                // Check for the spec version property.
-                var version = eventData["specversion"].Value<string>();
-                if (!string.IsNullOrEmpty(version)) return true;
+                return false;
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-
-            return false;
+            var headers = Request.Headers["Content-Type"];
+            return headers.Contains("application/cloudevents");
         }
 
         #endregion
