ff4d8ac5dba0341b3778641579b5547bcaebd7dd
Ningyi K <ningyi.kuo@sg.ey.com>
2023-12-22 05:08:08 +0800

    add missing interface

---
+++ viewer/Models/IEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace viewer.Models
+{
+    public interface IEvent<T> where T : class
+    {
+        string Id { get; set; }
+        string Type { get; set; }
+        string Subject { get; set; }
+        DateTime Time { get; set; }
+        T Data { get; set; }
+    }
+}
